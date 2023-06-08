1c37fd2ff477887187a6868528e7df725113afc5
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-08 09:58:04 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -6,14 +6,6 @@ def dealCard():
     card = random.choice(cards) 
     return card
 
-userCards = []
-computerCards = []
-isGameOver = False
-
-for _ in range(2):
-    "Deals two cards to the user and the computer."
-    userCards.append(dealCard())
-    computerCards.append(dealCard())
 
 def calculateScore(cards):
     """Takes a list of cards and returns the score calculated from the cards."""
@@ -24,28 +16,64 @@ def calculateScore(cards):
         cards.append(1)
     return sum(cards)
 
-while not isGameOver:
-    "While the game is not over, the user can choose to get another card or pass."
-    userScore = calculateScore(userCards)
-    compScore = calculateScore(computerCards)
-
-    print(f"Your cards: {userCards}, current score: {userScore}")
-    print(f"Computers cards: {computerCards}, current score: {compScore}")
-
-    if userScore == 0 or compScore == 0 or userScore > 21:
-        isGameOver = True
+def compare(userScore, compScore):
+    """Compares the user's score and the computer's score and returns the winner."""
+    if userScore == compScore:
+        return "Draw"
+    elif userScore == 0:
+        return "Win with a Blackjack"
+    elif compScore == 0:
+        return "Lose, opponent has Blackjack"
+    elif userScore > 21:
+        return "You went over. You lose"
+    elif compScore > 21:
+        return "Opponent went over. You win"
+    elif userScore > compScore:
+        return "You win"
     else:
-        anotherCard = input("Type 'y' to get another card, type 'n' to pass: ")
-        if anotherCard == 'y':
-            userCards.append(dealCard())
-            userScore = calculateScore(userCards)
-        else:
+        return "You lose"
+
+
+def playGame():
+    """Plays the game of Blackjack."""
+    userCards = []
+    computerCards = []
+    isGameOver = False
+
+    for _ in range(2):
+        "Deals two cards to the user and the computer."
+        userCards.append(dealCard())
+        computerCards.append(dealCard())
+
+    while not isGameOver:
+        "While the game is not over, the user can choose to get another card or pass."
+        userScore = calculateScore(userCards)
+        compScore = calculateScore(computerCards)
+
+        print(f"Your cards: {userCards}, current score: {userScore}")
+        print(f"Computers cards: {computerCards}, current score: {compScore}")
+
+        if userScore == 0 or compScore == 0 or userScore > 21:
             isGameOver = True
+        else:
+            anotherCard = input("Type 'y' to get another card, type 'n' to pass: ")
+            if anotherCard == 'y':
+                userCards.append(dealCard())
+                userScore = calculateScore(userCards)
+            else:
+                isGameOver = True
 
-while compScore != 0 and compScore < 17:
-    "The computer will get another card if the score is less than 17."
-    computerCards.append(dealCard())
-    compScore = calculateScore(computerCards)
+    while compScore != 0 and compScore < 17:
+        "The computer will get another card if the score is less than 17."
+        computerCards.append(dealCard())
+        compScore = calculateScore(computerCards)
 
-print(f"Your final hand: {userCards}, final score: {userScore}")
-print(f"Computer's final hand: {computerCards}, final score: {compScore}")
+    print(f"Your final hand: {userCards}, final score: {userScore}")
+    print(f"Computer's final hand: {computerCards}, final score: {compScore}")
+    print(compare(userScore, compScore))
+
+    while input("Do you want to play again? (y/n):"):
+        "The user can choose to play again."
+        playGame()
+
+playGame()
